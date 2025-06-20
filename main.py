516afbb2cf9c8af19c5a666d6dd0f20b4f3d723f
kkshisuke <zhoudominique@gmail.com>
2025-06-20 10:21:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -94,4 +94,5 @@ def post_message(channel, text, thread_ts=None):
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
